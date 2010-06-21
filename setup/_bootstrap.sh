24fe5570e25dd6f633c941b773a546160bd11275
Corey Oordt <coordt@washingtontimes.com>
2010-06-21 14:03:37 -0400

    Updated the bootstrap file to handle the latest version of veritualenvwrapper

--- setup/_bootstrap.sh
+++ setup/_bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 VE_PATH=`which virtualenv`
-VEW_PATH=`which virtualenvwrapper_bashrc`
+VEW_PATH=`which virtualenvwrapper.sh`
 
 if [ -z $VE_PATH ]
 then
@@ -12,9 +12,12 @@ if [ -z $VEW_PATH ]
 then
     sudo easy_install virtualenvwrapper
     mkdir ~/.virtualenvs
-    echo "Put the following lines in your .bashrc, .profile or environment
-    export WORKON_HOME=\$HOME/.virtualenvs
-    source virtualenvwrapper_bashrc"
+    echo "Put the following lines in your shell startup file (.bashrc, .profile, etc.) to set the location where the virtual environments should live and the location of the script installed with this package:
+
+    export WORKON_HOME=$HOME/.virtualenvs
+    source virtualenvwrapper.sh
+    
+    After editing it, reload the startup file (e.g., run: source ~/.bashrc)."
 fi
 
 
